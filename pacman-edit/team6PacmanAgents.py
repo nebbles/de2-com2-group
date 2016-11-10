1d565cfb2aa8b67163261c6ee6f29f8738d3bedc
nebbles <bsg115@ic.ac.uk>
2016-11-10 00:03:51 +0000

    made copies of relevant classes and methods from ghost file

--- pacman-edit/team6PacmanAgents.py
+++ pacman-edit/team6PacmanAgents.py
@@ -1,12 +1,116 @@
 # myPacmanAgents.py
 
-from team6GhostAgents import *
 from pacman import Directions
 from game import Agent
 from game import GameStateData
 import random
 import game
+import numpy as np
 import util
+
+
+class Queue:
+    def __init__(self):
+        self.items = []
+    def isEmpty(self):
+        return self.items == []
+    def enqueue(self, item):
+        self.items.insert(0,item)
+    def dequeue(self):
+        return self.items.pop()
+    def size(self):
+        return len(self.items)
+
+
+def getPathAction(self, state, myPath): # take path and extract first move direction
+
+    legalActions = state.getLegalActions(self.index)  # get legal actions of self
+    currentPos = myPath[-1]  # current position is last item in list
+    nextPos = myPath[-2]  # position after current is penultimate item in list
+
+    east = [currentPos[0] + 1, currentPos[1]]
+    west = [currentPos[0] - 1, currentPos[1]]
+    north = [currentPos[0], currentPos[1] + 1]
+    south = [currentPos[0], currentPos[1] - 1]
+
+    # if next suggested position is 'direction' and 'direction' is a legal action then move 'direction'
+    if nextPos == north and Directions.NORTH in legalActions: return Directions.NORTH
+    if nextPos == south and Directions.SOUTH in legalActions: return Directions.SOUTH
+    if nextPos == east and Directions.EAST in legalActions: return Directions.EAST
+    if nextPos == west and Directions.WEST in legalActions: return Directions.WEST
+    else: return legalActions[0]  # else just fo the first legal option (stops attempted backward movements)
+
+
+def shortestPath(walls, start, end):
+    '''
+    SHORTEST PATH FUNCTION
+    returns path1,path2
+    path1 is shortest path and path2 is second shortest path
+    '''
+    start = [start[0], start[1]]
+    end = [end[0], end[1]]
+    if start == end:
+        return [start]
+
+    neighbours = Queue()  # queue storing the next positions to explore
+    neighbours.enqueue(start)
+    counts = np.zeros((walls.width, walls.height),
+                      dtype=int)  # 2D array to store the distance from the start to all visted points
+    predecessors = np.zeros((counts.shape[0], counts.shape[1], 2),
+                            dtype=int)  # 2D array storing the predecessors (past points allowing path to be retraced)
+    counts[start[0], start[1]] = 1
+
+    # loop until the end position is found
+    while not neighbours.isEmpty():
+        n = neighbours.dequeue()
+        # add all the valid neighbours to the list and remember from where they came from
+        if n != end:
+            for neighbour in [[n[0] - 1, n[1]], [n[0] + 1, n[1]], [n[0], n[1] - 1],
+                              [n[0], n[1] + 1]]:  # for all neighbors to current
+                if not walls[neighbour[0]][neighbour[1]] and counts[neighbour[0], neighbour[1]] == 0:
+                    neighbours.enqueue(neighbour)
+                    predecessors[neighbour[0], neighbour[1]] = n
+                    counts[neighbour[0], neighbour[1]] = counts[n[0], n[1]] + 1
+
+    if counts[end[0], end[1]] == 0:
+        return []  # path not found
+    path1 = []
+    path2 = []
+    n = end
+
+    # calculate alternate route start point for reconstruction
+    n = end
+    adjacent = [[end[0] + 1, end[1]], [end[0] - 1, end[1]], [end[0], end[1] + 1], [end[0], end[1] - 1]]
+    minoption = None
+
+    for option in adjacent:
+        if option != predecessors[end[0], end[1]].tolist() and not walls[option[0]][option[1]]:
+            if minoption == None: minoption = option  # for first iteration
+            if counts[minoption[0], minoption[1]] >= counts[option[0], option[1]]:
+                minoption = option  # update min option with a better min
+
+    # construct path 1
+    while n != start:
+        if n == start:
+            break
+        path1.append(n)
+        n = predecessors[n[0], n[1]].tolist()
+    path1.append(start)
+
+    # construct path 2
+    n = minoption
+    while n != start:
+        if n == start:
+            break
+        path2.append(n)
+        n = predecessors[n[0], n[1]].tolist()
+    path2.append(start)
+    path2.insert(0, end)
+
+    # print 'counts \n', counts  # debug
+    # print 'predecessors \n', predecessors  # debug
+
+    return path1, path2
 
 
 def wallsConverted(state):
